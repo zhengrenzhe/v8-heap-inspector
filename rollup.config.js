fcdee384b83ff22e4025431c6a40404329a9588e
zhengrenzhe <zhengrenzhe0416@outlook.com>
2023-07-24 22:09:33 +0800

    rc

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import copy from "rollup-plugin-copy";
+import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "cli/bin.ts",
+  output: {
+    dir: "cli-output",
+    format: "cjs",
+    entryFileNames: "[name].cjs",
+  },
+  external(id) {
+    if (/\.node$/.test(id)) return true;
+    return false;
+  },
+  plugins: [
+    typescript(),
+    commonjs(),
+    json(),
+    copy({
+      targets: [{ src: "*.node", dest: "cli-output" }],
+    }),
+  ],
+};
+
+// xx
