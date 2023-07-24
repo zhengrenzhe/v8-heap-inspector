3d27c2935123193f87b1775b8ac60c08b21b5b6c
zhengrenzhe <zhengrenzhe0416@outlook.com>
2023-07-24 22:04:59 +0800

    bin version

--- cli/bin.ts
+++ cli/bin.ts
@@ -2,20 +2,27 @@
 
 import arg from "arg";
 import path from "path";
+import chalk from "chalk";
 
-import { Local } from "./cli";
+import { Local } from "./local";
 
 const args = arg({
   // Types
   "--local": Boolean,
   "--file": String,
   "--port": Number,
+  "--version": Boolean,
 
   // Aliases
   "-l": "--local",
   "-f": "--file",
   "-p": "--port",
+  "-v": "--version",
 });
+
+if (args["--version"]) {
+  console.log(chalk.green(require("../package.json").version));
+}
 
 if (args["--local"] && args["--file"]) {
   const filePath = path.isAbsolute(args["--file"])
